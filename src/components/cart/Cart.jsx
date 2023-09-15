d26374b4ca90f0504e6a69eba99491aa7079e0a1
masum9100 <masum.seopro@gmail.com>
2023-09-16 01:59:53 +0600

    credit and price added into cart and second toast added when total credit will be more than 20

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const Cart = ({ selectedCourse }) => {
-    console.log(selectedCourse)
+const Cart = ({ selectedCourse,leftCredit, totalCredit, totalPrice }) => {
+    
     return (
         <div>
-            <div className='w-72 bg-white p-4 rounded-md'>
-                <h1 className='my-3 text-lg font-bold text-[#2F80ED] '>Credit Hour Remaining 20 hr</h1>
+            <div className='sm:w-52 md:w-60 lg:w-72 bg-white p-4 rounded-md'>
+                <h1 className='my-3 text-lg font-bold text-[#2F80ED] '>Credit Hour Remaining {leftCredit} hr</h1>
                 <hr />
                 <h1 className='my-3 text-xl font-bold text-[#1C1B1B]'>Course Name</h1>
                 {
                     selectedCourse.map((course, index) => (
-                        <p key={index}> {index + 1}. {course.title}</p>))
+                        <p className='text-[#777676] font-normal text-base' key={index}> {index + 1}. {course.title}</p>))
                 }
                 <hr />
-                <p className='my-3 text-base font-medium text-[#494949]'>Total Credit Hour : </p>
+                <p className='my-3 text-base font-medium text-[#494949]'>Total Credit Hour : {totalCredit}</p>
                 <hr />
-                <p className='mt-3 text-base font-semibold text-[#494949]'>Total Price :  USD</p>
+                <p className='mt-3 text-base font-semibold text-[#494949]'>Total Price : {totalPrice} USD</p>
 
             </div>
         </div>
